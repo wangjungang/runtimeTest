964d167448441a2576388b9d321ccf5e70c147c7
wangjungang <1499370499@qq.com>
2018-07-31 23:59:47 +0800

    add - first

---
+++ runtimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtimeTest
+//
+//  Created by 王俊钢 on 2018/7/31.
+//  Copyright © 2018年 wangjungang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtimeTest
+//
+//  Created by 王俊钢 on 2018/7/31.
+//  Copyright © 2018年 wangjungang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
